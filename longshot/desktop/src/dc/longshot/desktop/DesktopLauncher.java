7da582eeb0dca1f5820f6cc60a55308535c1b323
David Chen <dchen5085@gmail.com>
2014-06-07 16:15:54 -0400

    the gradles

---
+++ longshot/desktop/src/dc/longshot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dc.longshot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dc.longshot.LongshotGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LongshotGame(), config);
+	}
+}
