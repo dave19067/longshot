3cea86010c5125f9c56e8fb3181d6c0d26a3f2f0
David Chen <dchen5085@gmail.com>
2014-11-23 21:41:48 -0500

    new gui

--- longshot/core/src/dc/longshot/game/Skins.java
+++ longshot/core/src/dc/longshot/game/Skins.java
@@ -7,8 +7,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public final class Skins {
-	
-	public static final Skin defaultSkin = new Skin(Gdx.files.internal("ui/default/uiskin.json"));
+
+	public static final Skin defaultSkin = new Skin(Gdx.files.internal("ui/default/defaultskin.json"));
+	//public static final Skin defaultSkin = new Skin(Gdx.files.internal("ui/test/uiskin.json"));
 	public static final BitmapFont ocrFont = new BitmapFont(Gdx.files.internal("ui/ocr/ocr.fnt"));
 	public static final LabelStyle ocrStyle = new LabelStyle(ocrFont, Color.WHITE);
 	
