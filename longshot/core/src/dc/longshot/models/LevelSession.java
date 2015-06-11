ac3d4e2ac3a61400ae57882dbdc8c3ae007e6259
David Chen <dchen5085@gmail.com>
2015-06-10 21:05:09 -0400

    Added all waves

--- longshot/core/src/dc/longshot/models/LevelSession.java
+++ longshot/core/src/dc/longshot/models/LevelSession.java
@@ -11,7 +11,7 @@ public final class LevelSession {
 		return executionState;
 	}
 	
-	public final void setExecutionState(ExecutionState executionState) {
+	public final void setExecutionState(final ExecutionState executionState) {
 		this.executionState = executionState;
 	}
 	
